8a1429544d22098020c97f5aa450c0d5502a5bb3
David <dmartin5150@icloud.com>
2022-05-23 19:18:47 -0500

    conditional new expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -76,6 +76,7 @@ return (
       <input type="date" onChange={dateChangeHandler} value={enteredDate} min="2019-01-01" max="2022-12-31"/>
     </div>
     <div className="new-expense__actions">
+      <button type="button" onClick={props.onCancel}>Cancel</button>
       <button type="submit">Add Expense</button>
     </div>
   </div>
